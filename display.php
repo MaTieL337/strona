b91a7ebacdb42199051bc7da6582d1ad13f382c1
Mikeal. <77624159+Michallesss@users.noreply.github.com>
2024-10-09 09:32:29 +0200

    work in progress

--- display.php
+++ display.php
@@ -4,11 +4,21 @@
     
   // Create connection
   $conn = new mysqli($host, $username, $password, $database);
+  $cena = 0;
   
   // Check connection
   if ($conn->connect_error) {
     die("Błąd serwera: " . $conn->connect_error);
   }
+
+  $query = "SELECT auto.rejestracja, auto.model, auto.marka, klient.imie, klient.nazwisko, klient.tel, naprawa.id_naprawa, naprawa.data, naprawa.zalecenia , naprawa.przebieg
+          FROM auto 
+          INNER JOIN klient ON auto.klient = klient.id_klient
+          INNER JOIN naprawa ON auto.id_auto = naprawa.auto 
+          WHERE naprawa.id_naprawa=1";
+
+  $result = $conn->query($query);
+  $row = $result->fetch_assoc();
 
   // Create pdf
   $pdf = new FPDF('P', 'mm', 'A4');
@@ -32,17 +42,17 @@
   
   $pdf->SetFont('Arial', '', 10);
 
-  $pdf->Cell(130, 5, 'Near DAV', 0, 0);
-  $pdf->Cell(25, 5, 'Customer ID:', 0, 0);
-  $pdf->Cell(34, 5, '123456', 0, 1);
+  $pdf->Cell(130, 5, $row['imie']." ".$row['nazwisko'], 0, 0);
+  $pdf->Cell(25, 5, 'Rejestracja:', 0, 0);
+  $pdf->Cell(34, 5, $row['rejestracja'], 0, 1);
 
-  $pdf->Cell(130, 5, 'City, State', 0, 0);
-  $pdf->Cell(25, 5, 'Invoice Date:', 0, 0);
-  $pdf->Cell(34, 5, '12/12/2019', 0, 1);
+  $pdf->Cell(130, 5, 'Telefon: '.$row['tel'], 0, 0); // zrobić osobne rowy na Telefon: i {telefon}
+  $pdf->Cell(25, 5, 'Data naprawy:', 0, 0);
+  $pdf->Cell(34, 5, $row['data'], 0, 1);
 
-  $pdf->Cell(130, 5, '', 0, 0);
-  $pdf->Cell(25, 5, 'Invoice No:', 0, 0); 
-  $pdf->Cell(24, 5, 'ORD01', 0, 1);
+  // $pdf->Cell(130, 5, '', 0, 0);
+  // $pdf->Cell(25, 5, 'Invoice No:', 0, 0); 
+  // $pdf->Cell(24, 5, 'ORD01', 0, 1);
 
   $pdf->SetFont('Arial', 'B', 15);
   $pdf->Cell(130, 5, 'Bill To:', 0, 0);
@@ -55,9 +65,9 @@
   $pdf->SetFont('Arial', 'B', 10);
   
   $pdf->Cell(10, 6, 'Sl', 1, 0, 'C');
-  $pdf->Cell(80, 6, 'Description', 1, 0, 'C');
+  $pdf->Cell(80, 6, 'Opis', 1, 0, 'C');
   $pdf->Cell(23, 6, 'Quantity', 1, 0, 'C');
-  $pdf->Cell(30, 6, 'Price', 1, 0, 'C');
+  $pdf->Cell(30, 6, 'Cena', 1, 0, 'C');
   $pdf->Cell(20, 6, 'Total', 1, 0, 'C');
   $pdf->Cell(25, 6, 'Total', 1, 1, 'C');
 
@@ -73,7 +83,7 @@
   }
 
   $pdf->Cell(118, 6, '', 0, 0);
-  $pdf->Cell(25, 6, 'Subtotal', 0, 0);
+  $pdf->Cell(25, 6, 'Suma', 0, 0);
   $pdf->Cell(45, 6, '15100.00', 1, 1, 'R');
   
   $pdf->Output();
