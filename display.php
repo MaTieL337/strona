ab7c82f4a0aea65fd25cb4e5dc85d4c595a3ffa2
Mikeal <mwiemszra@gmail.com>
2024-10-11 21:19:41 +0200

    mechanika

--- display.php
+++ display.php
@@ -15,7 +15,7 @@
           FROM auto 
           INNER JOIN klient ON auto.klient = klient.id_klient
           INNER JOIN naprawa ON auto.id_auto = naprawa.auto 
-          WHERE naprawa.id_naprawa=1";
+          WHERE naprawa.id_naprawa=".$_GET['id'];
 
   $result = $conn->query($query);
   $row = $result->fetch_assoc();
@@ -38,24 +38,28 @@
 
   $pdf->Cell(71, 5, 'Klient', 0, 0);
   $pdf->Cell(59, 5, '', 0, 0);
-  $pdf->Cell(59, 5, 'Details', 0, 1);
+  $pdf->Cell(59, 5, 'Szczegóły', 0, 1);
   
   $pdf->SetFont('Arial', '', 10);
 
-  $pdf->Cell(130, 5, $row['imie']." ".$row['nazwisko'], 0, 0);
-  $pdf->Cell(25, 5, 'Rejestracja:', 0, 0);
-  $pdf->Cell(34, 5, $row['rejestracja'], 0, 1);
+  $pdf->Cell(130, 5, $row['imie'], 0, 0);
+  $pdf->Cell(25, 5, 'Rejestracja: '.$row['rejestracja'], 0, 0);
+  $pdf->Cell(34, 5, '', 0, 1);
 
-  $pdf->Cell(130, 5, 'Telefon: '.$row['tel'], 0, 0); // zrobić osobne rowy na Telefon: i {telefon}
-  $pdf->Cell(25, 5, 'Data naprawy:', 0, 0);
-  $pdf->Cell(34, 5, $row['data'], 0, 1);
+  $pdf->Cell(130, 5, $row['nazwisko'], 0, 0); // zrobić osobne rowy na Telefon: i {telefon}
+  $pdf->Cell(25, 5, 'Model: '.$row['model'], 0, 0);
+  $pdf->Cell(34, 5, '', 0, 1);
 
-  // $pdf->Cell(130, 5, '', 0, 0);
-  // $pdf->Cell(25, 5, 'Invoice No:', 0, 0); 
-  // $pdf->Cell(24, 5, 'ORD01', 0, 1);
+  $pdf->Cell(130, 5, 'Telefon: '.$row['tel'], 0, 0);
+  $pdf->Cell(25, 5, 'Marka: '.$row['marka'], 0, 0); 
+  $pdf->Cell(24, 5, '', 0, 1);
+  
+  $pdf->Cell(130, 5, 'Data: '.$row['data'], 0, 0);
+  $pdf->Cell(25, 5, 'Przebieg: '.$row['przebieg'], 0, 0); 
+  $pdf->Cell(24, 5, '', 0, 1);
 
   $pdf->SetFont('Arial', 'B', 15);
-  $pdf->Cell(130, 5, 'Bill To:', 0, 0);
+  $pdf->Cell(130, 5, '', 0, 0);
   $pdf->Cell(59, 5, '', 0, 0);
   $pdf->SetFont('Arial', 'B', 10);
   $pdf->Cell(189, 10, '', 0, 1);
@@ -64,27 +68,27 @@
   
   $pdf->SetFont('Arial', 'B', 10);
   
-  $pdf->Cell(10, 6, 'Sl', 1, 0, 'C');
-  $pdf->Cell(80, 6, 'Opis', 1, 0, 'C');
-  $pdf->Cell(23, 6, 'Quantity', 1, 0, 'C');
-  $pdf->Cell(30, 6, 'Cena', 1, 0, 'C');
-  $pdf->Cell(20, 6, 'Total', 1, 0, 'C');
-  $pdf->Cell(25, 6, 'Total', 1, 1, 'C');
+  $pdf->Cell(10, 6, 'Lp.', 1, 0, 'C');
+  $pdf->Cell(133, 6, 'Opis', 1, 0, 'C');
+  $pdf->Cell(45, 6, 'Cena', 1, 0, 'C');
+  $pdf->Cell(0, 6, '', 0, 1, 'C');
 
+  $query = "SELECT * FROM cena WHERE naprawa = ".$_GET['id'];
+  $result = $conn->query($query);
+  $i = 1;
 
   $pdf->SetFont('Arial', '', 10);
-  for ($i = 0; $i <= 10; $i++) {
-    $pdf->Cell(10, 6, '1', 1, 0);
-    $pdf->Cell(80, 6, 'Product 1', 1, 0);
-    $pdf->Cell(23, 6, '1', 1, 0, 'R');
-    $pdf->Cell(30, 6, '15000.00', 1, 0, 'R');
-    $pdf->Cell(20, 6, '100.00', 1, 0, 'R');
-    $pdf->Cell(25, 6, '15100.00', 1, 1, 'R');
+  while ($row = $result->fetch_assoc()) {
+    $pdf->Cell(10, 6, $i, 1, 0);
+    $pdf->Cell(133, 6, $row['opis'], 1, 0);
+    $pdf->Cell(45, 6, $row['cena'], 1, 1, 'R');
+    $i++;
+    $cena+=$row['cena'];
   }
 
-  $pdf->Cell(118, 6, '', 0, 0);
-  $pdf->Cell(25, 6, 'Suma', 0, 0);
-  $pdf->Cell(45, 6, '15100.00', 1, 1, 'R');
+  $pdf->Cell(131, 6, '', 0, 0);
+  $pdf->Cell(12, 6, 'Suma', 1, 0);
+  $pdf->Cell(45, 6, $cena, 1, 1, 'R');
   
   $pdf->Output();
   $conn->close();
